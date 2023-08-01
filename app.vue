6e3f430179d9b9698ebaf55b8afe03f65a104315
Joel Yap <jy202019@gmail.com>
2023-08-01 21:15:48 +1200

    Added basic UI

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
+  <div style="background-color: lightgrey">
+    <h2>Mushroom Identification</h2>
+    <input v-model="text">
+  </div>
   <div>
-    <NuxtWelcome />
+    <h2>Advanced Search</h2>
+    <h3>Size</h3>
+    <input v-model="text">
+    <h3>Colour</h3>
+    <input v-model="text">
+    <h3>Environment</h3>
+    <input v-model="text">
+    <h3>Time of Year</h3>
+    <input v-model="text">
+    <br>
+    <button>Apply Filters</button>
+  </div>
+  <hr>
+  <div>
+    
+    <h2>Mushroom Name</h2>
+    <h3>Edibility: Safe</h3>
+    <button>View Details</button>
   </div>
 </template>
