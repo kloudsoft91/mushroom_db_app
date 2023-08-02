f433db5040cdc47fd859b708c6d6656958e4f53a
DiscountHomebrand <rmhall92@gmail.com>
2023-08-02 15:07:22 +1200

    Added page directory, deleted app.vue (index now acts as app.vue), added landing page and modified css for filter bar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+export default {
+  // Configuration options here
+  devtools: { enabled: true },
+  routes: [
+    {
+      name: 'home',
+      path: '/',
+      component: 'pages/index.vue'
+    },
+    {
+      name: 'about',
+      path: '/about',
+      component: 'pages/about.vue'
+    },
+  ],
+}
---
+++ pages/index.vue
@@ -0,0 +1,196 @@
+
+<template>
+    <div>
+      <!-- Navbar -->
+      <nav class="navbar">
+        <div class="navbar-links">
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+        </div>
+        <div class="navbar-header">
+          <h1 class="navbar-title">Mushroom Finder</h1>
+        </div>
+        
+        <div class="navbar-search">
+          <input type="text" placeholder="Search...">
+          <button>Search</button>
+        </div>
+      </nav>
+  
+      <!-- Main content -->
+      <div class="main-content">
+        <!-- Filter panel -->
+        <aside class="filter-panel">
+          <div id="mushroom-identification">
+            <h2>Mushroom Identification</h2>
+            <input v-model="text">
+          </div>
+          <div id="advanced-search">
+            <h2>Advanced Search</h2>
+            <h3>Size</h3>
+            <input v-model="text">
+            <h3>Colour</h3>
+            <input v-model="text">
+            <h3>Environment</h3>
+            <input v-model="text">
+            <h3>Time of Year</h3>
+            <input v-model="text">
+            <br>
+            <button>Apply Filters</button>
+          </div>
+          <hr>
+          <div id="mushroom-name">
+            <h2>Mushroom Name</h2>
+            <h3>Edibility: Safe</h3>
+            <button>View Details</button>
+          </div>
+        </aside>
+  
+        <!-- Main content section -->
+        <main class="landing-content">
+          <!-- Landing page content here -->
+          <h1>Welcome to Mushroom Finder</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            malesuada, neque eu luctus dictum, libero urna lobortis quam, quis
+            facilisis massa lectus vel dui.
+          </p>
+        </main>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+    /* Global styles */
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+    }
+  
+    /* Navbar styles */
+    .navbar {
+      background-color: #7e6551;
+      color: #fff;
+      display: flex;
+      align-items: center;
+      padding: 1rem;
+      justify-content: space-between;
+    }
+    
+    .navbar-header {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+    .navbar-title {
+      display: flex;
+      text-align: center;
+      justify-content: center;
+    }
+  
+    .navbar-links {
+      display: flex;
+      gap: 1rem;
+      margin-left: 1rem;
+    }
+  
+    .navbar-links a {
+      color: #fff;
+      text-decoration: none;
+      padding: 0.5rem 1rem;
+      border-radius: 5px;
+    }
+  
+    .navbar-links a:hover {
+      background-color: #603e2c;
+    }
+  
+    .navbar-search {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+    }
+  
+    .navbar-search input {
+      padding: 0.5rem;
+      border-radius: 5px;
+      border: 1px solid #603e2c;
+      flex: 1;
+      margin-right: 1rem;
+    }
+  
+    .navbar-search button {
+      padding: 0.5rem 1rem;
+      border-radius: 5px;
+      background-color: #603e2c;
+      color: #fff;
+      border: none;
+      cursor: pointer;
+    }
+  
+    .navbar-search button:hover {
+      background-color: #482b1f;
+    }
+
+    
+  
+    /* Filter panel styles */
+    .filter-panel {
+      background-color: #ede6dd;
+      padding: 1rem;
+    }
+  
+    .filter-panel div {
+      background-color: #f5f1ec;
+      padding: 1rem;
+      margin-bottom: 1rem;
+    }
+  
+    .filter-panel h2 {
+      margin-bottom: 0.5rem;
+    }
+  
+    .filter-panel input {
+      padding: 0.5rem;
+      border-radius: 5px;
+      border: 1px solid #ccc;
+      width: 93%;
+      margin: 0 auto;
+    }
+  
+    .filter-panel button {
+      padding: 0.5rem 1rem;
+      border-radius: 5px;
+      background-color: #603e2c;
+      color: #fff;
+      border: none;
+      cursor: pointer;
+      width: 100%;
+      margin-top: 0.5rem
+    }
+  
+    .filter-panel button:hover {
+      background-color: #482b1f;
+    }
+  
+    /* Main content styles */
+    .main-content {
+      display: flex;
+      gap: 2rem;
+      max-width: 100%;
+      padding: 2rem;
+    }
+  
+    .landing-content {
+      flex: 1;
+      background-color: #fff;
+      padding: 2rem;
+      border: 1px solid #ccc;
+    }
+  </style>
+  
