0b0f024725604a1e892fae9c4c05c2515d463c91
Emily <emily_clare@live.com>
2023-08-29 19:54:38 +0200

    adds images to each

--- components/ResultCards.vue
+++ components/ResultCards.vue
@@ -3,11 +3,11 @@
       <div v-for="(item, index) in mushroomData" :key="index"
            class="block max-w-[18rem] rounded-lg bg-white shadow-[0 2px 15px -3px rgba(0,0,0,0.07),0 10px 20px -2px rgba(0,0,0,0.04)] dark:bg-neutral-700">
         <div class="relative overflow-hidden bg-cover bg-no-repeat">
-          <img class="rounded-t-lg" :src="item.image" alt="" />
+          <img class="rounded-t-lg" src="./mushy.jpg" alt="" /> <!--src="item.image"-->
         </div>
         <div class="p-6">
           <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-            {{ item.title }}
+            {{ item.common_names }}
           </h5>
           <p class="text-base text-neutral-600 dark:text-neutral-200">
             {{ item.description }}
@@ -21,10 +21,10 @@
         </ul>
         <div class="p-6">
           <a type="button" href="#" class="pointer-events-auto mr-5 inline-block cursor-pointer rounded text-base font-normal leading-normal text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:text-primary-700">
-            Card Link
+            Details
           </a>
           <a type="button" href="#" class="pointer-events-auto inline-block cursor-pointer rounded text-base font-normal leading-normal text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:text-primary-700">
-            Another Link
+            Lookalikes
           </a>
         </div>
       </div>
