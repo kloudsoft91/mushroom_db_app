521f970e45890c4ec1ceb920ae47ce6b30b41a93
Emily <emily_clare@live.com>
2023-09-12 16:43:16 +0200

    added index.vue and sampledata from time of test creation

--- pages/index.vue
+++ pages/index.vue
@@ -1,198 +1,264 @@
 
 <template>
-    <div>
-      <!-- Navbar -->
-      <nav class="navbar">
-        <div class="navbar-links">
-          <a href="/">Home</a>
-          <a href="/about">About</a>
-        </div>
-        <div class="navbar-header">
-          <h1 class="navbar-title">Mushroom Finder</h1>
-        </div>
-        
-        <div class="navbar-search">
-          <input type="text" placeholder="Search...">
-          <button>Search</button>
-        </div>
-      </nav>
-  
-      <!-- Main content -->
-      <div class="main-content">
-        <!-- Filter panel -->
-        <aside class="filter-panel">
-          <div id="mushroom-identification">
-            <h2>Mushroom Identification</h2>
-            <input v-model="text">
-          </div>
-          <div id="advanced-search">
-            <h2>Advanced Search</h2>
-            <h3>Size</h3>
-            <input v-model="text">
-            <h3>Colour</h3>
-            <input v-model="text">
-            <h3>Environment</h3>
-            <input v-model="text">
-            <h3>Time of Year</h3>
-            <input v-model="text">
-            <br>
-            <button>Apply Filters</button>
-          </div>
-          <hr>
-          <div id="mushroom-name">
-            <h2>Mushroom Name</h2>
-            <h3>Edibility: Safe</h3>
-            <button>View Details</button>
-          </div>
-        </aside>
-  
-        <!-- Main content section -->
-        <main class="landing-content">
-          <!-- Landing page content here -->
-          <h1>Welcome to Mushroom Finder</h1>
-          <p>
-            There are lots of interesting mushrooms to discover all around where we live. I've seen at least 10
-            different species in my back yard the last month or so. Only one of those is edible. Mushroom
-            identification is tricky. There are often only subtle differences between something delicious and a day
-            or two of "severe gastrointestinal distress". One to two species can kill you, but it's a week before
-            you start to feel symptoms.
-          </p>
-        </main>
+  <div>
+    <!-- Navbar -->
+    <nav class="navbar">
+      <div class="navbar-links">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
       </div>
-    </div>
-  </template>
-  
-  <script setup>
-  </script>
-  
-  <style scoped>
-    /* Global styles */
-    body {
-      font-family: Arial, sans-serif;
-      margin: 0;
-      padding: 0;
-    }
-  
-    /* Navbar styles */
-    .navbar {
-      background-color: #7e6551;
-      color: #fff;
-      display: flex;
-      align-items: center;
-      padding: 1rem;
-      justify-content: space-between;
-    }
-    
-    .navbar-header {
-    flex: 1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-    .navbar-title {
-      display: flex;
-      text-align: center;
-      justify-content: center;
-    }
-  
-    .navbar-links {
-      display: flex;
-      gap: 1rem;
-      margin-left: 1rem;
-    }
-  
-    .navbar-links a {
-      color: #fff;
-      text-decoration: none;
-      padding: 0.5rem 1rem;
-      border-radius: 5px;
-    }
-  
-    .navbar-links a:hover {
-      background-color: #603e2c;
-    }
-  
-    .navbar-search {
-      display: flex;
-      align-items: center;
-      gap: 1rem;
-    }
-  
-    .navbar-search input {
-      padding: 0.5rem;
-      border-radius: 5px;
-      border: 1px solid #603e2c;
-      flex: 1;
-      margin-right: 1rem;
-    }
-  
-    .navbar-search button {
-      padding: 0.5rem 1rem;
-      border-radius: 5px;
-      background-color: #603e2c;
-      color: #fff;
-      border: none;
-      cursor: pointer;
-    }
-  
-    .navbar-search button:hover {
-      background-color: #482b1f;
-    }
+      <div class="navbar-header">
+        <h1 class="navbar-title">Mushroom Finder</h1>
+      </div>
+      
+      <div class="navbar-search">
+        <input type="text" placeholder="Search...">
+        <button>Search</button>
+      </div>
+    </nav>
 
-    
+    <!-- Main content -->
+    <div class="main-content">
+      <!-- Filter panel -->
+      <aside class="filter-panel">
+        <div id="mushroom-identification">
+          <h2>Mushroom Identification</h2>
+          <input v-model="text">
+        </div>
+        <div id="advanced-search">
+          <h2>Advanced Search</h2>
+          <h3>Size</h3>
+          <input v-model="text2">
+          <h3>Colour</h3>
+          <input v-model="text3">
+          <h3>Environment</h3>
+          <input v-model="text4">
+          <h3>Time of Year</h3>
+          <input v-model="text5">
+          <button @click="searchMushrooms">Test Search Button</button>
+          <h3>Tags</h3>
+          <select v-model="selectedTags" multiple>
+            <option v-for="tag in availableTags" :key="tag" :value="tag">{{ capitalize(tag) }}</option>
+          </select>
+          <br>
+          <button @click="filterMushrooms">Apply Filters</button>
+        </div>
+        <hr>
+        <div id="mushroom-name">
+          <h2 v-if="filteredMushrooms.length > 0">Filtered Results</h2>
+            <ul v-if="filteredMushrooms.length > 0">
+              <li v-for="mushroom in filteredMushrooms" :key="mushroom.id">
+                <h2>{{ mushroom.common_names }}</h2>
+                <p>{{ mushroom.description }}</p>
+                <p>Edibility: {{ mushroom.edibility }}</p>
+                <button>View Details</button>
+              </li>
+            </ul>
+            <p v-else>No filter selected</p>
+        </div>
+      </aside>
+
+      <!-- Main content section -->
+      <main class="landing-content">
+        <!-- Landing page content here -->
+        <h1>Welcome to Mushroom Finder</h1>
+        <p>
+          There are lots of interesting mushrooms to discover all around where we live. I've seen at least 10
+          different species in my back yard the last month or so. Only one of those is edible. Mushroom
+          identification is tricky. There are often only subtle differences between something delicious and a day
+          or two of "severe gastrointestinal distress". One to two species can kill you, but it's a week before
+          you start to feel symptoms.
+        </p>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import mushroomData from '~/data/sampledata.js'
+
+const mushrooms = ref([]);
+const filteredMushrooms = ref([]);
+const selectedTags = ref([]);
+
+const text = ref("");
+const text2 = ref("");
+const text3 = ref("");
+const text4 = ref("");
+const text5 = ref("");
+
+//tag data
+const tagInput = ref('');
+//predefined tags
+const tagOptions = ["edible", "poisonous", "psychoactive"]
+
+//assign data to 'mushrooms' array
+onMounted(() => {
+  mushrooms.value = mushroomData;
+});
+
+//applies tag filters
+const applyTagFilter = () => {
+  return mushrooms.value.filter((mushroom) => 
+  selectedTags.value.every((tag) => mushroom.tags.includes(tag))
+  );
+};
+
+//passes tag inputs to applyTagFilter
+const filterMushrooms = () => {
+  //no tags selected -> display all mushrooms
+  if(selectedTags.value.length === 0){
+    filteredMushrooms.value = mushrooms.value;
+  } else{
+    filteredMushrooms.value = applyTagFilter();
+  }
+};
+
+//List of predefined tags
+const availableTags = ['edible', 'poisonous', 'psychoactive']
+
+//Capitalize tags for user without changing data
+const capitalize = (str) => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+const searchMushrooms = () => {
+  var data = JSON.parse(JSON.stringify(mushrooms.value));
+  //The filter function should include d.JSONObject.includes(constValue) and || OR or && AND to add more search parameters. Includes() is just for string objects.
+  var searchedMushrooms = data.filter(d => (d.common_names.includes(text.value) || d.latin_names.includes(text.value)) && d.cap_features.diameter.includes(text2.value) && d.cap_features.colour.includes(text3.value) && d.environment.includes(text4.value));
+  filteredMushrooms.value = searchedMushrooms;
+  console.log(text2.value);
+};
+</script>
+
+<style scoped>
+  /* Global styles */
+  body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+
+  /* Navbar styles */
+  .navbar {
+    background-color: #7e6551;
+    color: #fff;
+    display: flex;
+    align-items: center;
+    padding: 1rem;
+    justify-content: space-between;
+  }
   
-    /* Filter panel styles */
-    .filter-panel {
-      background-color: #ede6dd;
-      padding: 1rem;
-    }
+  .navbar-header {
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+  .navbar-title {
+    display: flex;
+    text-align: center;
+    justify-content: center;
+  }
+
+  .navbar-links {
+    display: flex;
+    gap: 1rem;
+    margin-left: 1rem;
+  }
+
+  .navbar-links a {
+    color: #fff;
+    text-decoration: none;
+    padding: 0.5rem 1rem;
+    border-radius: 5px;
+  }
+
+  .navbar-links a:hover {
+    background-color: #603e2c;
+  }
+
+  .navbar-search {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .navbar-search input {
+    padding: 0.5rem;
+    border-radius: 5px;
+    border: 1px solid #603e2c;
+    flex: 1;
+    margin-right: 1rem;
+  }
+
+  .navbar-search button {
+    padding: 0.5rem 1rem;
+    border-radius: 5px;
+    background-color: #603e2c;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+  }
+
+  .navbar-search button:hover {
+    background-color: #482b1f;
+  }
+
   
-    .filter-panel div {
-      background-color: #f5f1ec;
-      padding: 1rem;
-      margin-bottom: 1rem;
-    }
-  
-    .filter-panel h2 {
-      margin-bottom: 0.5rem;
-    }
-  
-    .filter-panel input {
-      padding: 0.5rem;
-      border-radius: 5px;
-      border: 1px solid #ccc;
-      width: 93%;
-      margin: 0 auto;
-    }
-  
-    .filter-panel button {
-      padding: 0.5rem 1rem;
-      border-radius: 5px;
-      background-color: #603e2c;
-      color: #fff;
-      border: none;
-      cursor: pointer;
-      width: 100%;
-      margin-top: 0.5rem
-    }
-  
-    .filter-panel button:hover {
-      background-color: #482b1f;
-    }
-  
-    /* Main content styles */
-    .main-content {
-      display: flex;
-      gap: 2rem;
-      max-width: 100%;
-      padding: 2rem;
-    }
-  
-    .landing-content {
-      flex: 1;
-      background-color: #fff;
-      padding: 2rem;
-      border: 1px solid #ccc;
-    }
-  </style>
-  
+
+  /* Filter panel styles */
+  .filter-panel {
+    background-color: #ede6dd;
+    padding: 1rem;
+  }
+
+  .filter-panel div {
+    background-color: #f5f1ec;
+    padding: 1rem;
+    margin-bottom: 1rem;
+  }
+
+  .filter-panel h2 {
+    margin-bottom: 0.5rem;
+  }
+
+  .filter-panel input {
+    padding: 0.5rem;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    width: 93%;
+    margin: 0 auto;
+  }
+
+  .filter-panel button {
+    padding: 0.5rem 1rem;
+    border-radius: 5px;
+    background-color: #603e2c;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+    margin-top: 0.5rem
+  }
+
+  .filter-panel button:hover {
+    background-color: #482b1f;
+  }
+
+  /* Main content styles */
+  .main-content {
+    display: flex;
+    gap: 2rem;
+    max-width: 100%;
+    padding: 2rem;
+  }
+
+  .landing-content {
+    flex: 1;
+    background-color: #fff;
+    padding: 2rem;
+    border: 1px solid #ccc;
+  }
+</style>
