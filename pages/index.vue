6f81f16067a513a47b1884e4693f0b726e4c1eb2
AdamN2424 <adamnormanjames@gmail.com>
2023-09-22 13:43:42 +1200

    refactored Index to use <script> tag

--- pages/index.vue
+++ pages/index.vue
@@ -16,88 +16,88 @@
   <BottomFrame />
 </template>
 
-<script setup>
-import { ref, onMounted } from 'vue'
+<script>
+import { ref, watch } from 'vue'
 import mushroomData from '~/data/sampledata.js'
 import MushroomCard from '~/components/MushroomCard.vue';
 import { searchInput } from '../store/store.js';
 
-//Load data
-onMounted(() => {
-  mushrooms.value = mushroomData;
-  //This will display all mushrooms in results initially 
-  filteredMushrooms.value = mushrooms.value;
-});
+export default{
+  data(){
+    return{
+      mushrooms: ref([]),
+      filteredMushrooms: ref([]),
+      selectedTags: ref([]),
+    };
+  },
 
-//JSON data
-const mushrooms = ref([]);
-const filteredMushrooms = ref([]);
+  methods:{
 
-//Inputs
-const selectedTags = ref([]);
-
-// This watch effect reactively triggers applyAllfilters when the search input changes
-watch(searchInput, (newSearchInput) => {
-  applyAllFilters(newSearchInput);
-});
-
-//Apply all of the Filters (Have to decide when this is called
-//currently called on filter button press, tag select, and when typing in Name search
-const applyAllFilters = (newSearchInput) => {
-    let results = mushrooms.value;
+    //Apply all of the Filters (Have to decide when this is called
+    //currently called on filter button press, tag select, and when typing in Name search
+    applyAllFilters(newSearchInput) {
+    let results = this.mushrooms;
     //pull results from each filter function
-    results = filterByTags(results);
-    results = filterByName(results, newSearchInput);
+    results = this.filterByTags(results);
+    results = this.filterByName(results, newSearchInput);
     //assign results to filteredMushrooms array
-    filteredMushrooms.value = results;
-    //debug log 
-    console.log('Filtered Results:', filteredMushrooms.value.map(mushroom => mushroom.common_names));
-    console.log("Selected tags: ", selectedTags.value);
-    console.log("Search input: ", searchInput.value);
+    if(this.filteredMushrooms){
+      this.filteredMushrooms = results;
+      //debug log 
+      console.log('Filtered Results:', this.filteredMushrooms.map(mushroom => mushroom.common_names));
+    }
+    console.log("Selected tags: ", this.selectedTags);
+    //console.log("Search input: ", searchInput.value);
+    },
+    //Populates selectedTags[] by input (currently toggles on/off, so you can have multiple tags on, will need to style buttons to show that a tag is toggled/pressed)
+    //Note: this function is not responsible for changing the results data
+    toggleTag(tag) {
+      const index = this.selectedTags.indexOf(tag);
+      if (index === -1) {
+        this.selectedTags.push(tag);
+      } else {
+        this.selectedTags.splice(index, 1);
+      }
+      this.applyAllFilters();
+    },
+    //Filters results based on current selectedTags[]
+    filterByTags(data) {
+    if (this.selectedTags.length === 0) {
+      return data;
+    }
+    return data.filter((mushroom) =>
+      this.selectedTags.every((tag) => mushroom.tags.includes(tag))
+    );
+    },
+    //Reactive Name Filter
+    filterByName(data, newSearchInput) {
+    //if no input - no data change
+    if (!newSearchInput) {
+      console.log("NO NAME SEARCH INPUT = ", newSearchInput)
+      return data;
+    }
+    console.log("NAMES SEARCH: ", newSearchInput)
+    //otherwise filter data by input against latin & common names
+    return data.filter(d =>
+      d.common_names.toLowerCase().includes(newSearchInput.toLowerCase()) ||
+      d.latin_names.toLowerCase().includes(newSearchInput.toLowerCase())
+    );
+    },
+  },
+  //Load data
+  mounted() {
+    this.mushrooms = mushroomData;
+    //display all results initially
+    this.filteredMushrooms = this.mushrooms;
+    //watch effect reactively trigger applyAllFilters
+    watch(searchInput, (newSearchInput) => {
+      this.applyAllFilters(newSearchInput);
+    });
+    this.applyAllFilters();
+  },
 };
-
-//Populates selectedTags[] by input (currently toggles on/off, so you can have multiple tags on, will need to style buttons to show that a tag is toggled/pressed)
-//Note: this function is not responsible for changing the results data
-const toggleTag = (tag) => {
-  const index = selectedTags.value.indexOf(tag);
-  if (index === -1) {
-    selectedTags.value.push(tag);
-  } else {
-    selectedTags.value.splice(index, 1);
-  }
-  applyAllFilters();
-};
-
-//Filters results based on current selectedTags[]
-const filterByTags = (data) => {
-  if (selectedTags.value.length === 0) {
-    return data;
-  }
-  return data.filter((mushroom) =>
-    selectedTags.value.every((tag) => mushroom.tags.includes(tag))
-  );
-};
-
-//Reactive Name Filter
-const filterByName = (data, newSearchInput) => {
-  //if no input - no data change
-  if (!newSearchInput) {
-    console.log("NO NAME SEARCH INPUT = ", newSearchInput)
-    return data;
-  }
-  console.log("NAMES SEARCH: ", newSearchInput)
-  //otherwise filter data by input against latin & common names
-  return data.filter(d =>
-    d.common_names.toLowerCase().includes(newSearchInput.toLowerCase()) ||
-    d.latin_names.toLowerCase().includes(newSearchInput.toLowerCase())
-  );
-};
-
-// Call applyFilters initially to show all mushrooms
-applyAllFilters();
 </script>
   
-  <style scoped>
-
-  </style>
+<style scoped>
+</style>
   
