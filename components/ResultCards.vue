250d345b8b4849faf3f22a95c0da474c71e8e33a
Emily <emily_clare@live.com>
2023-09-23 16:04:11 +0200

    link to mushroom details page

--- components/ResultCards.vue
+++ components/ResultCards.vue
@@ -34,7 +34,7 @@
                 </tr>
             </tbody></table>
           </div>
-          <!-- Pop up for further details -->
+          <!-- Link to single mushroom-detail page -->
           <div class="p-4 ">
             <div class="flex justify-center space-x-4">
               <NuxtLink to="/mushroom-detail" class="pointer-events-auto inline-block cursor-pointer text-base font-normal leading-normal bg-emerald-700 text-white rounded-full px-4 py-2 hover:bg-emerald-600 focus:bg-purple-600">
@@ -56,41 +56,13 @@
         </div>
       </div>
     </div>
-    <MushroomDetailModal
-    v-if="showPopup"
-    :mushroom="selectedMushroom"
-    @close-popup="closePopup"
-  />
   </template>
   
   <script>
-  import MushroomDetailModal from './MushroomDetailModal.vue';
-
+ 
   export default {
     props: {
       filteredMushrooms: Object,
-    },
-    data() {
-      return {
-        // Add state variables for the popup
-        showPopup: false,
-        selectedMushroom: null,
-      };
-    },
-    methods: {
-      // open the popup and set the selected mushroom
-      openPopup(mushroom) {
-        this.selectedMushroom = mushroom;
-        this.showPopup = true;
-      },
-      // close the popup
-      closePopup() {
-        this.showPopup = false;
-        this.selectedMushroom = null;
-      },
-    },
-    components: {
-      MushroomDetailModal,
     },
   };
 </script>
