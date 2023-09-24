013686a1395ef26b6f59632f7c3cb4fa2ac3904a
Emily <emily_clare@live.com>
2023-09-24 19:08:05 +0200

    initial test updates

--- index.test.js
+++ index.test.js
@@ -1,14 +1,16 @@
 import { mount } from '@vue/test-utils';
-import MushroomFinder from 'pages/index.vue';
+import Index from 'pages/index.vue';
 
 
-describe('MushroomFinder', () => {
+describe('Index', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = mount(MushroomFinder);
-  });
+    wrapper = mount(Index);
+    jest.mock('./assets/images/mushy.jpg', () => 'Photo by Egor Kamelev from Pexels: https://www.pexels.com/photo/brown-mushroom-on-ground-1685650/');
 
+  });
+  
   it('should filter based on selected tags', () => {
     const mockMushrooms = [
       { id: 1, common_names: 'Mushroom 1', tags: ['edible'] },
@@ -16,12 +18,11 @@ describe('MushroomFinder', () => {
       { id: 3, common_names: 'Mushroom 3', tags: ['edible'] },
     ];
 
-    // Select a tag
     wrapper.vm.selectedTags = ['edible'];
     
     // Trigger filterByTags method
     const filteredMushrooms = wrapper.vm.filterByTags(mockMushrooms);
-   
+    
     // Check if filtered mushrooms match the selected tag
     expect(filteredMushrooms).toHaveLength(2);
     expect(filteredMushrooms[0].common_names).toBe('Mushroom 1');
@@ -63,10 +64,10 @@ describe('MushroomFinder', () => {
     ];
 
     // Set search inputs
-    wrapper.vm.nameSearch = 'Shaggy';
+    wrapper.vm.searchInput = "shaggy";
 
     // Trigger filterByName method
-    const filteredMushrooms = wrapper.vm.filterByName(mockMushrooms);
+    const filteredMushrooms = wrapper.vm.filterByName(mockMushrooms, wrapper.vm.searchInput);
 
     // Check if mushrooms are filtered based on name provided
     expect(filteredMushrooms).toHaveLength(1);
@@ -88,10 +89,10 @@ describe('MushroomFinder', () => {
     ];
 
     // Set search inputs
-    wrapper.vm.nameSearch = 'Amanita';
+    wrapper.vm.searchInput = 'Amanita';
 
     // Trigger filterByName method
-    const filteredMushrooms = wrapper.vm.filterByName(mockMushrooms);
+    const filteredMushrooms = wrapper.vm.filterByName(mockMushrooms, wrapper.vm.searchInput);
 
     // Check if mushrooms are filtered based on latin name
     expect(filteredMushrooms).toHaveLength(1);
@@ -114,10 +115,10 @@ describe('MushroomFinder', () => {
     ];
 
     // Set search inputs
-    wrapper.vm.nameSearch = "";
+    wrapper.vm.searchInput = "";
 
     // Trigger filterByName method
-    const filteredMushrooms = wrapper.vm.filterByName(mockMushrooms);
+    const filteredMushrooms = wrapper.vm.filterByName(mockMushrooms, wrapper.vm.searchInput);
 
     // Check all mushrooms are displayed
     expect(filteredMushrooms).toHaveLength(2);
@@ -125,7 +126,38 @@ describe('MushroomFinder', () => {
     expect(filteredMushrooms[1].id).toBe(2);
   })
 
-  it('should display all if no range value is provided', () => {
+  it('should filter mushrooms based on selected cap shape', () => {
+    const mockMushrooms = [
+      { id: 1, common_names: 'Mushroom 1', cap_features: { shape: 'convex' } },
+      { id: 2, common_names: 'Mushroom 2', cap_features: { shape: 'flat' } },
+      { id: 3, common_names: 'Mushroom 3', cap_features: { shape: 'convex' } },
+    ];
+    
+    wrapper.vm.selectedCapShape = 'convex';
+   
+    const filteredMushrooms = wrapper.vm.filterByCapShape(mockMushrooms);
+
+    expect(filteredMushrooms).toHaveLength(2);
+    expect(filteredMushrooms[0].common_names).toBe('Mushroom 1');
+    expect(filteredMushrooms[1].common_names).toBe('Mushroom 3');
+  });
+
+  it('should return all mushrooms when selected cap shape is null', () => {
+    const mockMushrooms = [
+      { id: 1, common_names: 'Mushroom 1', cap_features: { shape: 'convex' } },
+      { id: 2, common_names: 'Mushroom 2', cap_features: { shape: 'flat' } },
+      { id: 3, common_names: 'Mushroom 3', cap_features: { shape: 'convex' } },
+    ];
+
+
+    wrapper.vm.selectedCapShape = null;
+
+    const filteredMushrooms = wrapper.vm.filterByCapShape(mockMushrooms);
+
+    expect(filteredMushrooms).toHaveLength(3);
+  });
+
+  it('should display all if no size is provided', () => {
     const mockMushrooms = [
       { 
         id: 1, 
@@ -140,7 +172,7 @@ describe('MushroomFinder', () => {
     ];
 
     // Trigger filterByRange method
-    const filteredMushrooms = wrapper.vm.filterByRange(mockMushrooms);
+    const filteredMushrooms = wrapper.vm.filterBySize(mockMushrooms);
 
     // Check all mushrooms are displayed
     expect(filteredMushrooms).toHaveLength(2);
@@ -148,7 +180,7 @@ describe('MushroomFinder', () => {
     expect(filteredMushrooms[1].id).toBe(2);
   })
 
-  it('should filter based on stipe features range', () => {
+  it('should filter based on size of stipe', () => {
     const mockMushrooms = [
       { 
         id: 1, 
@@ -163,14 +195,14 @@ describe('MushroomFinder', () => {
     ];
 
     // Trigger filterByRange method
-    const filteredMushrooms = wrapper.vm.filterByRange(mockMushrooms, '5', 'stipe_features.diameter_min', 'stipe_features.diameter_max');
+    const filteredMushrooms = wrapper.vm.filterBySize(mockMushrooms, '5', 'stipe_features.diameter_min', 'stipe_features.diameter_max');
 
     // Check mushrooms are filtered based on given range
     expect(filteredMushrooms).toHaveLength(1);
     expect(filteredMushrooms[0].id).toBe(2);
   })
 
-  it('should display none found within stipe features range', () => {
+  it('should display none found within stipe size', () => {
     const mockMushrooms = [
       {
         id: 1, 
@@ -185,12 +217,12 @@ describe('MushroomFinder', () => {
     ];
 
     // Trigger filterByRange method
-    const filteredMushrooms = wrapper.vm.filterByRange(mockMushrooms, '9', 'stipe_features.diameter_min', 'stipe_features.diameter_max');
+    const filteredMushrooms = wrapper.vm.filterBySize(mockMushrooms, '9', 'stipe_features.diameter_min', 'stipe_features.diameter_max');
 
     expect(filteredMushrooms).toHaveLength(0);
   })
 
-  it('should filter based on cap features range', () => {
+  it('should filter based on size of cap', () => {
     const mockMushrooms = [
       { 
         id: 1, 
@@ -205,13 +237,13 @@ describe('MushroomFinder', () => {
     ];
 
     // Trigger filterByRange method
-    const filteredMushrooms = wrapper.vm.filterByRange(mockMushrooms, '3', 'cap_features.diameter_min', 'cap_features.diameter_max');
+    const filteredMushrooms = wrapper.vm.filterBySize(mockMushrooms, '3', 'cap_features.diameter_min', 'cap_features.diameter_max');
 
     expect(filteredMushrooms).toHaveLength(1);
     expect(filteredMushrooms[0].id).toBe(2);
   })
 
-  it('should display none found within cap features range', () => {
+  it('should display none found within cap size', () => {
     const mockMushrooms = [
       {
         id: 1, 
@@ -226,12 +258,56 @@ describe('MushroomFinder', () => {
     ];
 
     // Trigger filterByRange method
-    const filteredMushrooms = wrapper.vm.filterByRange(mockMushrooms, '9', 'cap_features.diameter_min', 'cap_features.diameter_max');
+    const filteredMushrooms = wrapper.vm.filterBySize(mockMushrooms, '9', 'cap_features.diameter_min', 'cap_features.diameter_max');
 
     expect(filteredMushrooms).toHaveLength(0);
   })
 
-  it('should display all if no stipe colour is provided', () => {
+  it('should apply all filters correctly', () => {
+    wrapper.vm.mushrooms = [
+      { 
+        id: 1, 
+        tags: ['edible'],
+        common_names: 'Mushroom 1',
+        latin_names: 'Mushroomus Maximus',
+        cap_features: {shape: 'flat'},
+      },
+      { 
+        id: 2, 
+        tags: ['edible'],
+        common_names: 'Fungus 1',
+        latin_names: 'Fungus Fleshius',
+        cap_features: {shape: 'convex'},
+      },
+      { 
+        id: 3, 
+        tags: ['edible'],
+        common_names: 'Mushroom 2',
+        latin_names: 'Magicus',
+        cap_features: {shape: 'convex'},
+      }
+    ];
+
+    // Set inputs
+    wrapper.vm.selectedTags = ['edible'];
+    wrapper.vm.searchInput = 'Mushroom';
+    wrapper.vm.selectedCapShape = 'convex';
+
+    // Trigger applyAllFilters method
+    wrapper.vm.applyAllFilters();
+
+    expect(wrapper.vm.filteredMushrooms).toHaveLength(1);
+    expect(wrapper.vm.filteredMushrooms[0].common_names).toBe('Mushroom 2');
+  }); 
+
+  
+/**
+ * 
+ * THESE METHODS AREN'T YET IMPLEMENTED IN INDEX.VUE SO WILL BE SKIPPED IN TEST FOR NOW
+ * 
+*/
+  
+  xit('should display all if no stipe colour is provided', () => {
     const mockMushrooms = [
       { 
         id: 1, 
@@ -259,7 +335,7 @@ describe('MushroomFinder', () => {
     expect(filteredMushrooms[2].id).toBe(3);
   })
 
-  it('should filter based on stipe colour', () => {
+  xit('should filter based on stipe colour', () => {
     const mockMushrooms = [
       { 
         id: 1, 
@@ -286,7 +362,7 @@ describe('MushroomFinder', () => {
     expect(filteredMushrooms[1].id).toBe(2);
   })
 
-  it('should display none found with stipe colour provided', () => {
+  xit('should display none found with stipe colour provided', () => {
     const mockMushrooms = [
       { 
         id: 1, 
@@ -311,7 +387,7 @@ describe('MushroomFinder', () => {
     expect(filteredMushrooms).toHaveLength(0);
   })
 
-  it('should display all if no cap colour is provided', () => {
+  xit('should display all if no cap colour is provided', () => {
     const mockMushrooms = [
       { 
         id: 1, 
@@ -339,7 +415,7 @@ describe('MushroomFinder', () => {
     expect(filteredMushrooms[2].id).toBe(3);
   })
 
-  it('should filter based on cap colour', () => {
+  xit('should filter based on cap colour', () => {
     const mockMushrooms = [
       { 
         id: 1, 
@@ -366,7 +442,7 @@ describe('MushroomFinder', () => {
     expect(filteredMushrooms[1].id).toBe(2);
   })
 
-  it('should display none found with cap colour provided', () => {
+  xit('should display none found with cap colour provided', () => {
     const mockMushrooms = [
       { 
         id: 1, 
@@ -391,10 +467,7 @@ describe('MushroomFinder', () => {
     expect(filteredMushrooms).toHaveLength(0);
   }) 
 
-
-  // PUT FILTER BY MONTH HERE
-
-  it('should return correct integer for the given month', () => {
+  xit('should return correct integer for the given month', () => {
     expect(wrapper.vm.monthToInt('january')).toBe(1);
     expect(wrapper.vm.monthToInt('february')).toBe(2);
     expect(wrapper.vm.monthToInt('march')).toBe(3);
@@ -409,12 +482,12 @@ describe('MushroomFinder', () => {
     expect(wrapper.vm.monthToInt('december')).toBe(12);
   });
 
-  it('should return undefined for an invalid month', () => {
+  xit('should return undefined for an invalid month', () => {
     expect(wrapper.vm.monthToInt('invalid')).toBeUndefined();
   });
 
 
-  it('should filter based on season', () => {
+  xit('should filter based on season', () => {
     const mockMushrooms = [
       {
         id: 1,
@@ -438,7 +511,7 @@ describe('MushroomFinder', () => {
     expect(filteredMushrooms[0].id).toBe(1);
   })
 
-  it('should filter based on month', () => {
+  xit('should filter based on month', () => {
     const mockMushrooms = [
       {
         id: 1,
@@ -462,7 +535,7 @@ describe('MushroomFinder', () => {
     expect(filteredMushrooms[0].id).toBe(1);
   })
 
-  it('should filter based on month within range', () => {
+  xit('should filter based on month within range', () => {
     const mockMushrooms = [
       {
         id: 1,
@@ -487,53 +560,5 @@ describe('MushroomFinder', () => {
     expect(filteredMushrooms[1].id).toBe(2);
   })
 
-  it('should apply all filters correctly', () => {
-    wrapper.vm.mushrooms = [
-      { 
-        id: 1, 
-        tags: ['edible'],
-        common_name: 'Mushroom 1',
-        latin_name: 'Mushroomus Maximus',
-        stipe_features: {diameter_min: '1', diameter_max: '3', colour: ['Brown']}, 
-        cap_features: {diameter_min: '1', diameter_max: '2', colour: ['Brown']},
-        time_of_year: 'Late spring to autumn (October to April in the Southern Hemisphere)'
-      },
-      { 
-        id: 2, 
-        tags: ['edible'],
-        common_name: 'Fungus 1',
-        latin_name: 'Fungus Fleshius',
-        stipe_features: {diameter_min: '1', diameter_max: '3', colour: ['Yellow']}, 
-        cap_features: {diameter_min: '1', diameter_max: '2', colour: ['Red']},
-        time_of_year: 'Late summer (March to June in the Southern Hemisphere)'
-      },
-      { 
-        id: 3, 
-        tags: ['edible'],
-        common_name: 'Mushroom 2',
-        latin_name: 'Magicus',
-        stipe_features: {diameter_min: '2', diameter_max: '5', colour: ['Brown']}, 
-        cap_features: {diameter_min: '1', diameter_max: '2', colour: ['Green']},
-        time_of_year: 'Late summer (March to June in the Southern Hemisphere)'
-      }
-    ];
 
-    // Set inputs
-    wrapper.vm.tags = ['edible'];
-    wrapper.vm.common_name = 'Mushroom';
-    wrapper.vm.stipeDiam = '2';
-    wrapper.vm.stipeLen = '';
-    wrapper.vm.stipeHgt = '';
-    wrapper.vm.capDiam = '1';
-    wrapper.vm.capThickness = '';
-    wrapper.vm.stipeColour = 'Brown';
-    wrapper.vm.capColour = '';
-    wrapper.vm.seasonSearch = 'summer';
-
-    // Trigger applyAllFilters method
-    wrapper.vm.applyAllFilters();
-
-    expect(wrapper.vm.filteredMushrooms).toHaveLength(1);
-    expect(wrapper.vm.filteredMushrooms[0].common_name).toBe('Mushroom 2');
-  });
 })
--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,30 @@
 module.exports = {
-    testEnvironmentOptions: {
-        customExportConditions: ["node", "node-addons"],
-     },
-    rootDir: "./",
-    modulePaths: [
-      "<rootDir>/"
-    ],
-    moduleNameMapper: {
-        '^~/(.*)$': '<rootDir>/$1',
-      },
-    moduleFileExtensions: [
-      'ts',
-      'js',
-      'vue',
-      'json'
-    ],
-    transform: {
-        '^.+\\.ts$': 'ts-jest',
-        '^.+\\.js$': 'babel-jest',
-        '^.+\\.vue$': '@vue/vue3-jest'
-      },
-    collectCoverage: true,
-    collectCoverageFrom: [
-      '<rootDir>/components/**/*.vue',
-      '<rootDir>/pages/**/*.vue'
-    ],
-    testEnvironment: 'jsdom'
-  }
+  testEnvironmentOptions: {
+      customExportConditions: ["node", "node-addons"],
+   },
+  rootDir: "./",
+  modulePaths: [
+    "<rootDir>/"
+  ],
+  moduleNameMapper: {
+      '^~/(.*)$': '<rootDir>/$1',
+      '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+      '\\.(jpg|jpeg|png|gif|webp|svg)$': 'identity-obj-proxy',
+    },
+  moduleFileExtensions: [
+    'ts',
+    'js',
+    'vue',
+    'json'
+  ],
+  transform: {
+      '^.+\\.ts$': 'ts-jest',
+      '^.+\\.js$': 'babel-jest',
+      '^.+\\.vue$': '@vue/vue3-jest'
+    },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/pages/index.vue'
+  ],
+  testEnvironment: 'jsdom'
+}
