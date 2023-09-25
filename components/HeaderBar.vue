5e9787e391b208125d1b0c91bbe67955ced72c7d
DiscountHomebrand <rmhall92@gmail.com>
2023-09-25 20:03:50 +1300

    added link to home clicking mushymatch icon top r

--- components/HeaderBar.vue
+++ components/HeaderBar.vue
@@ -2,7 +2,7 @@
   <header class="hidden tablet:hidden desktop:block bg-white content-center">
     <nav class="flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 w-3/4 m-auto">
       <div class="mb-2 flex flex-row self-center">
-          <h1 class="font-semibold text-emerald-500 text-lg tablet:text-xl desktop:text-3xl">Mushy Match</h1>
+        <NuxtLink to="/" class="font-semibold text-emerald-500 text-lg tablet:text-xl desktop:text-3xl">Mushy Match</NuxtLink>
       </div>
       <div class="sm:mb-0 self-center">
         <NuxtLink to="/" class="ml-5 text-emerald-700 hover:text-emerald-500 text-lg">Home</NuxtLink>
