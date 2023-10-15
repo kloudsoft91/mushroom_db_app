8ed1232c2a5ed001c0f23d6ec370b86a0b32faba
Emily <emily_clare@live.com>
2023-10-15 16:43:24 +0200

    rough formatting two columns

--- pages/mushroom-detail/[id].vue
+++ pages/mushroom-detail/[id].vue
@@ -2,25 +2,32 @@
     <HeaderBar />
     <NavigationBar @search="handleSearch" @tagFilter="handleTags" @sizeFilter="handleSizeFilter" />
     <FooterBar @openCarouselInputs="openCarouselInputs" />
-    <carousel :items-to-show="1">
-      <slide v-for="(image, index) in mushroomData.photos" :key="index">
-        <nuxt-img class="rounded-t-lg" provider="cloudinary" :src="`ar_3:4,c_fill,h_1024,w_768/${image}`" alt="Mushroom Image"></nuxt-img>
-      </slide>
+    <div class="grid grid-cols-1 tablet:grid-cols-2 laptop:grid-cols-2 desktop:grid-cols-2 ultra:grid-cols-2 gap-5 justify-stretch justify-items-center w-11/12 desktop:w-3/4 mx-auto">
+      <div class="image-carousel">
+        <carousel :items-to-show="1">
+        <slide v-for="(image, index) in mushroomData.photos" :key="index">
+          <nuxt-img class="rounded-t-lg" provider="cloudinary" :src="`ar_3:4,c_fill,h_1024,w_768/${image}`" alt="Mushroom Image"></nuxt-img>
+        </slide>
 
-      <template #addons>
-        <navigation />
-        <pagination />
-      </template>
-    </carousel>
-    <h1 class="text-3xl font-semibold my-4">{{ mushroomData.common_names.split(',').map(name => name.trim()).join(' / ') }}</h1>
-    <table class="mushroom-table">
-      <tbody>
-        <tr v-for="(value, key) in mushroomData" :key="key">
-          <td class="pr-4">{{ formatKey(key) }}</td>
-          <td>{{ formatValue(value) }}</td>
-        </tr>
-      </tbody>
-    </table>
+        <template #addons>
+          <navigation />
+          <pagination />
+        </template>
+      </carousel>
+      </div>
+      <div class="information-table">
+        <h1 class="text-3xl font-semibold my-4">{{ mushroomData.common_names.split(',').map(name => name.trim()).join(' / ') }}</h1>
+        <!-- Apply modern styling to the table -->
+        <table >
+          <tbody>
+            <tr v-for="(value, key) in mushroomData" :key="key">
+              <td>{{ formatKey(key) }}</td>
+              <td>{{ formatValue(value) }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
 </template>
     
 <script>
